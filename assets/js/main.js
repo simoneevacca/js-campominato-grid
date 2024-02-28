2024d5e08c4d9084aa6c38ce1a8541afbdf78247
simoneevacca <v.simone.vacca@gmail.com>
2024-02-28 14:08:22 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+// Ogni cella ha un numero progressivo, da 1 a 100.
+// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+
+// al click del bottone...
+
+// genero le 100 celle all'interno della griglia
+
+// assegno un numero ad ogni cella
+
+// al click della singola cella gli asegno una classe per dare il colore di sfondo
+// e stampare il numero in console
